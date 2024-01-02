10f8b5a2aef95f4813e55bbc8923d0ebfbaa60e1
Rohit-Deshbhratar <deshbhratarrohit9@gmail.com>
2024-01-02 10:44:35 +0530

    screenshot completed

--- test_cases/conftest.py
+++ test_cases/conftest.py
@@ -1,4 +1,8 @@
+import os.path
+import time
+
 import pytest
+import pytest_html
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.firefox.service import Service
@@ -6,6 +10,7 @@ from selenium.webdriver.firefox.service import Service
 
 @pytest.fixture(autouse=True)
 def setup(request, browser):
+    global driver
     if browser == "chrome":
         chrome_path = r"D:/MyThings/Development/Chrome_For_Testing/chrome-win64/chrome.exe"
         chrome_driver = r"D:/MyThings/Development/Chrome_For_Testing/chromedriver-win64/chromedriver.exe"
@@ -24,7 +29,7 @@ def setup(request, browser):
 
         driver = webdriver.Firefox(options=option, service=service)
 
-    driver.get("https://seleniumbase.io/demo_page")
+    driver.get("https://seleniumbase.io/demo_page02")
     driver.maximize_window()
     request.cls.driver = driver
     yield
@@ -32,9 +37,36 @@ def setup(request, browser):
 
 
 def pytest_addoption(parser):
-    parser.addoption("--browser", default="chrome")
+    parser.addoption("--browser", default="firefox")
 
 
 @pytest.fixture(scope="class", autouse=True)
 def browser(request):
     return request.config.getoption("--browser")
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item):
+    outcome = yield
+    report = outcome.get_result()
+    extras = getattr(report, "extras", [])
+    if report.when == "call":
+        # always add url to report
+        extras.append(pytest_html.extras.url("http://www.rcvacademy.com/"))
+        xfail = hasattr(report, "wasxfail")
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            # only add additional html on failure
+            report_directory = os.path.dirname(item.config.option.htmlpath)
+            # file_name = str(int(round(time.time() * 1000)))
+            file_name = report.nodeid.replace("::", "_") + ".png"
+            destination_file = os.path.join(report_directory, file_name)
+            driver.save_screenshot(destination_file)
+            if file_name:
+                html = '<div><img src="%s" alt="screenshot" style="width:300px;height=200px"' \
+                       'onclick="window.open(this.src)" align="right"/></div>' % file_name
+            extras.append(pytest_html.extras.html(html))
+        report.extras = extras
+
+
+def pytest_html_report_title(report):
+    report.title = "Selenium Base Report"
